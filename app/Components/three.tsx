51cd52d89774f1fc61a5b317fb8a1b65651b9b07
Prashant Dubey <pdubey1924@gmail.com>
2026-07-04 01:03:31 +0530

    three

--- app/Components/three.tsx
+++ app/Components/three.tsx
@@ -8,34 +8,43 @@ export default function Animate3() {
 
   useEffect(() => {
     if (!mountRef.current) return;
-
-    // Scene
     const scene = new THREE.Scene();
-
-    // Camera
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-
     camera.position.z = 5;
 
-    // Renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
+    console.log(renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // Inject canvas into React div
     mountRef.current.appendChild(renderer.domElement);
-
-    // Cube
+    
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshNormalMaterial();
+    const material = new THREE.MeshBasicMaterial({color:0x00ff00});
     const cube = new THREE.Mesh(geometry, material);
-
+    
     scene.add(cube);
+    const planeGeometry = new THREE.PlaneGeometry(5,10,10,5);
+    const PlaneMaterial = new THREE.MeshPhongMaterial({
+       
+        side : THREE.DoubleSide
+    })
 
+
+
+    const planeMesh = new THREE.Mesh(planeGeometry,PlaneMaterial)
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+    scene.add(ambientLight);
+
+     const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+      directionalLight.position.set(3, 3, 3);
+  console.log(directionalLight);
+     scene.add(directionalLight);
+    scene.add(planeMesh);
+    console.log(scene);
     function animate() {
       requestAnimationFrame(animate);
 
